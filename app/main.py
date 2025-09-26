58b9265e6bd9b42745fa45d979542ddb0306ffd1
Stephen Macharia <stephenmacharia024@gmail.com>
2025-09-26 09:25:48 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "*").split(","),
+    allow_origins=os.getenv("*", "*").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
